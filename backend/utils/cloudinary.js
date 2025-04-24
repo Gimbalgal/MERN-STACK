f8a1ac2c8df6d927743731822e3366cc3395391d
Victoria Ishabo <victoriaishabo55@gmail.com>
2025-04-24 11:49:33 +0100

    I just made sure that my postman is working fine with my cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2;
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
@@ -8,6 +9,5 @@ cloudinary.config({
 console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API Key:", process.env.CLOUDINARY_API_KEY);
 console.log("Secret Key:", process.env.CLOUDINARY_API_SECRET);
-
 
 module.exports = cloudinary;
