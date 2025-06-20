bb9efdc6da2d66f2b943a80ec1bd82baa54daf1a
Victoria Ishabo <victoriaishabo55@gmail.com>
2025-06-20 17:36:36 +0100

    I just made changes to my folder

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -3,7 +3,7 @@
 // export default App;
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Flower from './pages/AdminPage/flower';
+import Flower from './AdminPage/flower';
 import CustomNavbar from './Component/navbar'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
